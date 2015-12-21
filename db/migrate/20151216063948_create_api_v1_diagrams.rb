7f0def43323ce13972367e8f4e76bd828b92c0e3
Pawel Przylucki <pawel@przylucki.pl>
2015-12-21 16:07:21 +0100

    FIX correct db schema and seeds

--- db/migrate/20151216063948_create_api_v1_diagrams.rb
+++ db/migrate/20151216063948_create_api_v1_diagrams.rb
@@ -1,7 +1,7 @@
 class CreateApiV1Diagrams < ActiveRecord::Migration[5.0]
   def change
     create_table :api_v1_diagrams do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
